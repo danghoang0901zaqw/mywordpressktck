20980a899e518a985020c7b97b6e2ef4ba61e47b
hoangtd <danghoang0901zaqw@gmail.com>
2022-10-18 15:45:56 +0700

    tdhoangz

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://danghoang0901zaqw.github.io/mywordpressktck/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Ftest2%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://danghoang0901zaqw.github.io/mywordpressktck/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Ftest2%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://danghoang0901zaqw.github.io/mywordpressktck/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Ftest2%2Fwp-admin%2Fpost.php&reauth=1">https://danghoang0901zaqw.github.io/mywordpressktck/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Ftest2%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
